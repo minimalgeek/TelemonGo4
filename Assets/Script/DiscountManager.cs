04bb89a5a757d326e98059aca7740dd9cb987aee
Faragó Balázs <minimalgeek@gmail.com>
2017-01-22 22:24:54 +0100

    Initial commit

---
+++ Assets/Script/DiscountManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Discount
+{
+    public GameObject item;
+    public int discountPercentage;
+}
+public class DiscountManager : Singleton<DiscountManager>
+{
+
+    private const string WIN_TEXT_LEFT = "You earned ";
+    private const string WIN_TEXT_RIGHT = " discount for your next purchase!";
+
+    public GameObject congratulationsPanel;
+    public Text congratulationsMessage;
+
+	public ParticleSystem winParticle;
+
+    public List<Discount> discounts;
+
+    public void PlayCongrats(GameObject item)
+    {
+        Discount found = discounts.Find(x => x.item == item);
+        if (found != null)
+        {
+            congratulationsPanel.SetActive(true);
+            congratulationsMessage.text = WIN_TEXT_LEFT + found.discountPercentage + WIN_TEXT_RIGHT;
+            congratulationsPanel.GetComponent<Image>().CrossFadeAlpha(0.0f, 2.0f, false);
+			congratulationsMessage.GetComponent<Text>().CrossFadeAlpha(0.0f, 2.0f, false);
+
+			Destroy(item);
+            StartCoroutine(PlayPS());
+        }
+    }
+
+    private IEnumerator PlayPS() {
+        winParticle.Play();
+        yield return new WaitForSeconds(2f);
+        congratulationsPanel.SetActive(false);
+    }
+
+}
---
+++ Assets/Script/ImageOnDrag.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.Utility;
+
+[RequireComponent(typeof(RectTransform))]
+public class ImageOnDrag : MonoBehaviour
+{
+    private RectTransform rect;
+	public DragRigidbody dragger;
+
+
+    public void OnPointerEnter(UnityEngine.EventSystems.BaseEventData eventData)
+    {
+        if (dragger.DraggedObject != null) {
+			GameObject dragged = dragger.DraggedObject;
+			Debug.Log("Object collected: " + dragged.name);
+			DiscountManager.Instance.PlayCongrats(dragged);
+		}
+    }
+}
